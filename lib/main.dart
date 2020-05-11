616eb9d794f064c3f27879e58c0d0587c3a9673e
Kamla Junior <alkaj09@gmail.com>
2020-05-11 17:27:21 +0100

    Cleaning the codebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
-import 'dart:math';
-
 import 'package:bmi_calculator/utils/constants.dart';
-import 'package:bmi_calculator/components/reusable_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import 'utils/bmi_brain.dart';
-import 'components/first_row_child.dart';
 import 'pages/input_page.dart';
 
 void main() => runApp(BMICalculator());
